9cb9435532edbb4a0efd0db9c16d3852afa11dfa
Angelinne <kayumi85@hotmail.com>
2022-11-13 08:36:11 +0900

    update main

--- kayumi_cub3d/cub3d.h
+++ kayumi_cub3d/cub3d.h
@@ -1,15 +1,17 @@
-
 /* # include "mlx_linux/mlx.h"
 # include "mlx_linux/mlx.h" */
+# include "libft/libft.h"
 # include "get_next_line/get_next_line.h"
 # include <math.h>
 # include <stdio.h>
 # include <string.h>
 
+#define ZERO '0'
+
 typedef struct s_map_info
 {
 	char	*line;
 	int		fd;
-	size_t	map_height;//高さ
-	size_t	map_width; //幅
+	size_t	height;
+	size_t	width;
 }t_map_info;
--- kayumi_cub3d/main.c
+++ kayumi_cub3d/main.c
@@ -1,17 +1,54 @@
 #include "cub3d.h"
 
-int main(int argc, char *argv[])
+/* void	split_line(t_map_info *map)
 {
-	t_map_info map;
 	int	i;
+	int	atoi_i;
+	char **str;
+	
+	i = 0;
+	atoi_i = (int *)malloc(sizeof(int));
+			str = ft_split(map->line, '\n');
+			while (str[i] != NULL)
+			{
+				atoi_i[i] = ft_atoi(str[i]);
+				printf("%d\n", atoi_i[i]);
+				free(str[i]);
+				i++;
+			}
+			free(str);
+} */
 
-	(void)argc;
-	map.fd = open(argv[1], O_RDONLY);
+int	find_0(t_map_info *map)
+{
+
+}
+
+void	read_get_line_map(char *argv[], t_map_info *map)
+{
+
+	map->fd = open(argv[1], O_RDONLY);
 	while(1)
 	{
-		map.line = get_next_line(map.fd);
-		printf("line is %s", map.line);
-		if (map.line == NULL)
+		map->line = get_next_line(map->fd);
+		printf("%s", map->line);
+		if (map->line == NULL)
 			break;
+			if(ft_strchr(map->line, ZERO) != 0)
+			{
+				printf("found!\n");
+			}
+			else
+			printf("not found\n");
+			free(map->line);
 	}
+}
+
+int main(int argc, char *argv[])
+{
+	t_map_info *map;
+
+	map = (t_map_info *)malloc(sizeof(t_map_info));//忘れがち
+	read_get_line_map(argv, map);
+	return (0);
 }
