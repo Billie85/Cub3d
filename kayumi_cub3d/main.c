fce664dc831edd3792758cfca24b3062ce06f5b2
Angelinne <kayumi85@hotmail.com>
2022-11-23 19:20:42 +0900

    get height and width

--- kayumi_cub3d/main.c
+++ kayumi_cub3d/main.c
@@ -1,29 +1,60 @@
 #include "cub3d.h"
 
+size_t largest_word(char *s[100])
+{
+	size_t	i;
+	size_t	len;
+	size_t	longest;
+	size_t	j;
+
+	longest = ft_strlen(s[3]);
+	printf("%ld\n", longest);
+	 i = 1;
+	while(i < len)
+	{
+		len = ft_strlen(s[i]);
+		if (longest < len)
+		{
+			longest = len;
+			j = i;
+		}
+		i++;
+	}
+	//printf("%s", s[i]);
+	return(longest);
+}
+
 int main(int argc, char *argv[])
 {
 	t_map_info *map;
 	int i;
-	int j;
-	int count = 0;
-
+	size_t len;
+	size_t max_width;
+	size_t max_height;
+	
+	i = 0;
+	len = 0;
+	max_width = 0;
+	max_height = 0;
 	map = (t_map_info *)malloc(sizeof(t_map_info));
 	map->fd = open(argv[1], O_RDONLY);
-
-	i = 0;
 	while(1)
 	{
 		map->array_2d[i] = get_next_line(map->fd);
 		if (map->array_2d[i] == NULL)
 			break;
+		len = ft_strlen(map->array_2d[i]);
+		if (len > max_width)
+			max_width = len;
 		i++;
+		max_height++;
 	}
-	for (int x = 0; x < 6; x++)
+	for (int x = 0; x < max_height; x++)
 	{
-		for(int y = 0; y < 12; y++)
+		for(int y = 0; y < max_width -1; y++)
 		{
-			//if (map->array_2d[x][y] == '0')
-			/* {
+		/* 	if (map->array_2d[x][y] == '0')
+			{
 				printf("aaaaaaaaaaaa\n");
 			} */
 			printf("%c ", map->array_2d[x][y]);
--- kayumi_cub3d/test.c
+++ kayumi_cub3d/test.c
@@ -3,5 +3,12 @@
 
 int main(void)
 {
-	ft_strchr()
+	char *str1 = "abcdefg";
+	char str2[] = "abcdefg";
+
+	str2[2] = 'X';
+	str1 = "new_string";
+
+	printf("%s\n", str1);
+	printf("%s\n", str2);
 }
