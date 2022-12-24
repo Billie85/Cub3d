88d926f1912550ebf1c7834de0999104e0cc0d66
Bodu Lyu <blyu@c1r25s7.42tokyo.jp>
2022-12-25 05:58:47 +0900

    ...

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: blyu <blyu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 05:41:07 by blyu              #+#    #+#             */
-/*   Updated: 2022/12/25 05:41:08 by blyu             ###   ########.fr       */
+/*   Updated: 2022/12/25 05:56:03 by blyu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int	set_map(char *file);
 
 int	main(int argc, char *argv[])
 {
-	if (argc != 2)
+	if (argc != 2 || ft_strlen(argv[1]) < 4 \
+	|| ft_memcmp(argv[1] + ft_strlen(argv[1]) - 4, ".cub", 4))
 		return (printf("Error\n") * 0 + 1);
 	mlx(mlx_init());
 	win(mlx_new_window(mlx(0), DIS_W, DIS_H, "cub3d"));
